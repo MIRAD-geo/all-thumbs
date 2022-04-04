dc97be746f9fe796096e281eeba8c4c71a01e4c8
Diana Li <dianali9619@gmail.com>
2022-04-04 15:00:36 -0400

    react router set-up

--- src/App.js
+++ src/App.js
@@ -1,17 +1,53 @@
-import React from "react";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+  } from "react-router-dom";
+  import * as React from "react";
+ 
+  function App() {
+    return (
+      <BrowserRouter>
+        <div>
+          <h1>Hello, React Router!</h1>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 
-
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
+  function Home() {
     return (
       <>
-        <h1 className="text-4xl text-white bg-black">
-          Hello GeoDude
-        </h1>
+        <main>
+          <h2>Welcome to the homepage!</h2>
+          <p>You can do this, I believe in you.</p>
+        </main>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
       </>
     );
   }
-}
+  
+  function About() {
+    return (
+      <>
+        <main>
+          <h2>Who are we?</h2>
+          <p>
+            That feels like an existential question, don't you
+            think?
+          </p>
+        </main>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+      </>
+    );
+  }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./styles.css";
+import * as React from "react";
+import { render } from 'react-dom';
+import App from "./App.js";
 
-
-var mountNode = document.getElementById("app");
-ReactDOM.render(<App name="Jane" />, mountNode);
+render(<App />,document.getElementById("root"));
