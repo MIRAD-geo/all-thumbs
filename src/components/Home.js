72f3ce31a07b8ddb193ff61c47ed438ee66f1978
Diana Li <dianali9619@gmail.com>
2022-04-04 15:44:23 -0400

    add shop link

---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+function Home() {
+    return (
+      <>
+        <main>
+          <h2>Welcome to the homepage!</h2>
+          <p>You can do this, I believe in you.</p>
+        </main>
+        <nav>
+          <Link to="/about">About</Link>
+          <br/>
+          <Link to='/shop'>Shop</Link>
+        </nav>
+      </>
+    );
+  }
+
+  export default Home;
---
+++ src/components/Shop.js
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { Link } from "react-router-dom";
+
+function shop() {
+    return (
+      <>
+        <main>
+          <h2>Shop</h2>
+          <p>
+           this is where we buy plants
+          </p>
+        </main>
+        <nav>
+          <Link to="/">Back to Home</Link>
+        </nav>
+      </>
+    );
+  }
+
+export default Shop;
