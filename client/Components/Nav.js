69dd9ea5c16d748f3dd4bb2610c54428d1e5250d
Diana Li <dianali9619@gmail.com>
2022-04-05 15:13:11 -0400

    need pull

--- client/Components/Nav.js
+++ client/Components/Nav.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
 import Cart from './shopping-chart';
-import CheckoutPreview from './CheckoutPreview';
 
 function Nav() {
     return (
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const PORT = 1111;
+const PORT = 3000;
 
 const app = express();
 app.use(express.json());
