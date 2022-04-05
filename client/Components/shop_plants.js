e2a3a6b3329bc13a384cefea52a299b932e3f850
Diana Li <dianali9619@gmail.com>
2022-04-05 19:29:13 -0400

    nav in shop

--- client/Components/shop_plants.js
+++ client/Components/shop_plants.js
@@ -29,6 +29,8 @@ function Plants() {
     }, [])
 
     return (
+        <>
+        <Nav />
         <div className="bg-white">
             <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
                 <h2 className="sr-only">Products</h2>
@@ -63,6 +65,7 @@ function Plants() {
                 </div>
             </div>
         </div>
+        </>
     )
 }
 
