45391286112ad8e1b383c121cb178aae6932b155
Diana Li <dianali9619@gmail.com>
2022-04-05 23:01:24 -0400

    working shopping chart

---
+++ client/Components/CartItem.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeItem, updateQuantity } from '../Slices/cartSlice';
+
+const CartItem = ({ item, id }) => {
+
+    const dispatch = useDispatch();
+
+    const deleteItem = () => {
+        dispatch(removeItem(id));
+    };
+
+    const updateItemQuantity = (e) => {
+        const newQuantity = e.target.value;
+        dispatch(updateQuantity({ id, newQuantity }));
+    };
+
+    return (
+        <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+            <div class="flex w-2/5">
+
+                <div class="w-20">
+                    <img class="h-24" src={item.image} alt="plant image"/>
+
+                </div>
+                <div class="flex flex-col justify-between ml-4 flex-grow">
+                    <span class="font-bold text-sm">{item.name}</span>
+                    <p onClick={() => deleteItem()} class="font-semibold hover:text-red-500 text-gray-500 text-xs">Remove</p>
+                </div>
+            </div>
+            <div class="flex justify-center w-1/5">
+                <input class="mx-2 border text-center w-10" type="number" name="quantity" placeholder='1' value={item.quantity} min={1} max={100} onChange={(e) => updateItemQuantity(e)} />
+            </div>
+            <span class="text-center w-1/5 font-semibold text-sm">$ {item.price}</span>
+            <span class="text-center w-1/5 font-semibold text-sm">$ {item.price} </span>
+        </div>
+    );
+};
+
+export default CartItem;
--- client/Components/shop_plants.js
+++ client/Components/shop_plants.js
@@ -1,7 +1,8 @@
-
 import * as React from "react";
 import { useEffect, useState } from "react";
 import Nav from "./Nav";
+import { addItem } from '../Slices/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 function Plants() {
@@ -28,43 +29,65 @@ function Plants() {
         getPlants();
     }, [])
 
+    const dispatch = useDispatch();
+
+    const cart = useSelector((state) => state.cart)
+
+    const addCart = (item, price, count, image ) => {
+        dispatch(addItem({name:item, price:price, quantity:count, image:image }))
+        console.log('count', count)
+    }
+
     return (
         <>
-        <Nav />
-        <div className="bg-white">
-            <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-                <h2 className="sr-only">Products</h2>
+            <Nav />
+            <div className="bg-white">
+                <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+                    <h2 className="sr-only">Products</h2>
 
-                <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-                    {realplants.map((plants) => (
-                        <a key={plants.id} href={`http://localhost:8080/#/details/#/${plants.id}`} className="group">
-                            <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                                <img
-                                    src={plants.url}
-                                    alt='plants image'
-                                    className="w-full h-full object-center object-cover group-hover:opacity-75"
-                                />
+                    <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+                        {realplants.map((plants) => (
+                            <div>
+                                <a key={plants.id} href={`http://localhost:8080/#/details/#/${plants.id}`} className="group">
+                                    <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                                        <img
+                                            src={plants.url}
+                                            alt='plants image'
+                                            className="w-full h-full object-center object-cover group-hover:opacity-75"
+                                        />
+                                    </div>
+                                </a>
+                                <h3 className="mt-4 text-sm text-gray-700">{plants.common_name}</h3>
+                                <div className='flex flex-col md:flex-row justify-between items-center text-gray-900'>
+                                    <p className="mt-1 text-lg font-medium text-gray-900">$ {plants.price}</p>
+
+                                    <button onClick={() => addCart(plants.common_name, plants.price, 1, plants.url)} className='text-sm px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none'>Add To Cart</button>
+                                </div>
                             </div>
-                            <h3 className="mt-4 text-sm text-gray-700">{plants.common_name}</h3>
-                            <p className="mt-1 text-lg font-medium text-gray-900">$ {plants.price}</p>
-                        </a>
-                    ))}
-                    {fakeplants.map((plants) => (
-                        <a key={plants.id} href={`http://localhost:8080/#/details/#/${plants.id}`} className="group">
-                            <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                                <img
-                                    src={plants.url}
-                                    alt='plants image'
-                                    className="w-full h-full object-center object-cover group-hover:opacity-75"
-                                />
+                        ))}
+                        {fakeplants.map((plants) => (
+                            <div>
+                                <a key={plants.id} href={`http://localhost:8080/#/details/#/${plants.id}`} className="group">
+                                    <div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                                        <img
+                                            src={plants.url}
+                                            alt='plants image'
+                                            className="w-full h-full object-center object-cover group-hover:opacity-75"
+                                        />
+                                    </div>
+                                </a>
+                                <h3 className="mt-4 text-sm text-gray-700">{plants.common_name}</h3>
+                                <p className='text-amber-900 text-xs'><em>brown-thumb friendly</em></p>
+                                <div className='flex flex-col md:flex-row justify-between items-center text-gray-900'>
+                                    <p className="mt-1 text-lg font-medium text-gray-900">$ {plants.price}</p>
+                                    <button onClick={() => addCart(plants.common_name, plants.price, 1, plants.url)} className='text-sm px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none'>Add To Cart</button>
+                                </div>
+
                             </div>
-                            <h3 className="mt-4 text-sm text-gray-700">{plants.common_name}</h3>
-                            <p className="mt-1 text-lg font-medium text-gray-900">$ {plants.price}</p>
-                        </a>
-                    ))}
+                        ))}
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
